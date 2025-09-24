e1e8a66ebb7559b29d17ada98b3d4fbd1b265367
Ahmadullah Mukhlis <nasarimukhlis@gmail.com>
2025-09-24 17:15:24 +0430

    teh otp is created for the login page is fixed

---
+++ backend/app/Mail/OtpLoginMail.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class OtpLoginMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+        public $otp;
+
+    public $user;
+
+    public function __construct($user, $otp)
+    {
+        $this->user = $user;
+        $this->otp = $otp;
+    }
+
+    public function build()
+    {
+        return $this->subject('Your OTP Code for Login')
+            ->markdown('otp');
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'view.name',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
