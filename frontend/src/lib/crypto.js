4fd5a4c847bca2a8c33587a51663791d6c759ab6
Ahmadullah Mukhlis <nasarimukhlis@gmail.com>
2025-09-24 16:15:18 +0430

    teh probel m of the encryption int eh is fxed

--- frontend/src/lib/crypto.js
+++ frontend/src/lib/crypto.js
@@ -4,8 +4,8 @@ import CryptoJS from 'crypto-js'
 // get key from env
 const SECRET = import.meta.env.VITE_ENCRYPTION_KEY
 
-export function encrypt(email) {
-  return CryptoJS.AES.encrypt(email, SECRET).toString()
+export function encrypt(data) {
+  return CryptoJS.AES.encrypt(data, SECRET).toString()
 }
 
 export function decrypt(cipherText) {
