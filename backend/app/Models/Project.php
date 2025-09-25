6131d90267dc122e8d1ab011e329f0c19cb3afe4
ahmadullahmukhlis <nasarimukhlis@gmail.com>
2025-09-25 15:44:15 +0000

    the problem of the is fxed is fxed fot eh project

---
+++ backend/app/Models/Project.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\CausesActivity;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class Project extends Model
+{
+    use HasFactory, LogsActivity, CausesActivity;
+
+    protected $guarded = [];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+                        ->logOnlyDirty()
+                        ->logOnly(['*'])
+                        ->useLogName('Project')
+                        ->dontSubmitEmptyLogs()
+                        ->dontLogIfAttributesChangedOnly(['updated_at'])
+                        ;
+    }
+
+    public function getCreatedAtAttribute($value){
+        $ss = cache()->get('software-settings');
+        if(!$ss) return $value;
+        return Carbon::parse($value)->format(cache()->get('software-settings')->date_format.' h:i:s A');
+    }
+}
