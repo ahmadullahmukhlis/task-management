5334edb211d883d4cb8ec504d866d6c72747bda1
Ahmadullah Mukhlis <nasarimukhlis@gmail.com>
2025-09-27 11:27:07 +0430

    teh problem fo teh sif xed is fxed

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -94,4 +94,8 @@ class User extends Authenticatable
     {
         return $this->hasMany(UserVerification::class);
     }
+    public function userProject() : HasMany
+    {
+        return $this->hasMany(UserProject::class ,'user_id');
+    }
 }
