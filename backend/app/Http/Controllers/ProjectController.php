fa3929c6e6d8d11e56b15d9d1f3252f2fa29a6f7
ahmadullahmukhlis <nasarimukhlis@gmail.com>
2025-09-27 13:56:11 +0000

    teh problem of the is fxed is fxed

--- backend/app/Http/Controllers/ProjectController.php
+++ backend/app/Http/Controllers/ProjectController.php
@@ -16,7 +16,7 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = Project::get();
+        $projects = Project::where('created_by',auth()->id())->orWhereRelation('userProject','user_id',auth()->id())->get();
         return ProjectResource::collection($projects);
     }
 
