76f786c6540f1ece32fa567c06ec8e6ba7eec9f9
Ahmadullah Mukhlis <nasarimukhlis@gmail.com>
2025-09-27 11:24:26 +0430

    teh problem fothe is fxed is fxed

--- backend/app/Models/UserProject.php
+++ backend/app/Models/UserProject.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Carbon;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\CausesActivity;
@@ -30,5 +31,14 @@ class UserProject extends Model
         $ss = cache()->get('software-settings');
         if(!$ss) return $value;
         return Carbon::parse($value)->format(cache()->get('software-settings')->date_format.' h:i:s A');
+    }
+    public function project() : BelongsTo
+
+    {
+        return $this->belongsTo(Project::class,'project_id');
+    }
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class,'user_id');
     }
 }
